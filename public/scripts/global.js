9e9455f8fb2bfd90222b61e36e27bd4c847a51c0
Johnny Cassidy <developer@ioconic.dev>
2024-05-06 14:28:45 +0000

    Add scripts and styles for nav

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,1 +1,17 @@
 console.log('global.js');
+
+
+// responsive menu logic
+const hamburgerMenuButton = document.querySelector('.hamburger-menu-button');
+const closeMenuButton = document.querySelector('.close-menu-button');
+const responsiveNav = document.querySelector('.responsive-nav');
+
+const toggleResponsiveMenuButtons = () => {
+  hamburgerMenuButton.classList.toggle('hidden');
+  closeMenuButton.classList.toggle('hidden');
+  responsiveNav.classList.toggle('hidden');
+  console.log('hello nurse');
+}
+
+hamburgerMenuButton.addEventListener('click', toggleResponsiveMenuButtons);
+closeMenuButton.addEventListener('click', toggleResponsiveMenuButtons);
--- public/views/components/main_header.php
+++ public/views/components/main_header.php
@@ -2,5 +2,6 @@
   <a class="header-logo-link" href="/">
     <h1 class="main-heading">SuperSlimeTime</h1>
   </a>
-  <?php require_once "views/components/main-nav.html" ?>
+  <?php require_once "views/components/main_nav.html" ?>
+  <?php require_once "views/components/responsive_nav.html" ?>
 </header>
